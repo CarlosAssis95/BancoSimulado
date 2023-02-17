9b9689b1ff00f6b1effc1cec5ce8b847cc0b3a36
CarlosAssis <calos.dudu1502@gmail.com>
2023-02-17 17:37:09 -0300

    primeiro commit

---
+++ src/com/carlos/projetobancario/Pessoa.java
@@ -0,0 +1,8 @@
+package com.carlos.projetobancario;
+
+public class Pessoa {
+	
+	String nome;
+	String cpf;
+	double dataNascimento;
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Carlo
+ *
+ */
+module Banco {
+}
